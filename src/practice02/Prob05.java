c2e1d7441a5e0839241f9b8a6a9e775abbace4e7
BIT <BIT@DESKTOP-E23PFRN>
2018-12-27 18:01:47 +0900

    Prob5 Complete Change

--- src/practice02/Prob05.java
+++ src/practice02/Prob05.java
@@ -6,42 +6,46 @@ import java.util.Scanner;
 public class Prob05 {
 
 	public static void main(String[] args) {
-		Random random = new Random();
-		int num = random.nextInt(100)+1;
-		int checkNum = 0;
-		int start = 1;
-		int end = 100;
-
-		System.out.println("수를 결정하였습니다. 맞추어보세요\n"+start+"-"+end);
-
-		Scanner scanner = new Scanner(System.in);
 
 		while(true) {
-			checkNum++;
-			System.out.print(checkNum+">>");
-			int a = scanner.nextInt();
+			Random random = new Random();
+			int num = random.nextInt(100)+1;
+			int checkNum = 0;
+			int start = 1;
+			int end = 100;
 
-			if(num>a) {
+			System.out.println("수를 결정하였습니다. 맞추어보세요\n"+start+"-"+end);
 
-				start = a;
-				System.out.println("더 높게\n"+a+"-"+end);
-			} else if(num<a){
+			Scanner scanner = new Scanner(System.in);
 
-				end = a;
-				System.out.println("더 낮게\n"+start+"-"+a);
-			} else {
+			while(true) {
+				checkNum++;
+				System.out.print(checkNum+">>");
+				int a = scanner.nextInt();
 
-				System.out.print("맞았습니다.\n다시하시겠습니까(y/n)>>");
-				String answer = scanner.next();
-				if(answer.equals("y")) {
-					
+				if(num>a) {
+
+					start = a;
+					System.out.println("더 높게\n"+a+"-"+end);
+				} else if(num<a){
+
+					end = a;
+					System.out.println("더 낮게\n"+start+"-"+a);
 				} else {
-					break;
-				}
 
+					System.out.print("맞았습니다.\n다시하시겠습니까(y/n)>>");
+					break;
+
+				}
 			}
+			String answer = scanner.next();
+			if(answer.equals("n")) {
+				break;
+			} 
 		}
 
-	}
 
+	}
 }
+
+
