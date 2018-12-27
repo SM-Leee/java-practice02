b40cd16b47bea2b2f011b40d901d8f558decc763
BIT <BIT@DESKTOP-E23PFRN>
2018-12-27 11:34:03 +0900

    Prob1 Change

--- src/practice02/Prob01.java
+++ src/practice02/Prob01.java
@@ -1,9 +1,19 @@
 package practice02;
 
+import java.util.Scanner;
+
 public class Prob01 {
 
 	public static void main(String[] args) {
 		final int[] MONEYS = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1 };
+
+		System.out.print("금액 : ");
+		Scanner scanner = new Scanner(System.in);
+		int money = scanner.nextInt();
+
+		for(int i=0;i<10;i++) {
+			
+		}
 	}
 
 }
